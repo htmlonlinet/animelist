f46347eeda05c8bde72cc799b17675e673ccc939
saturnLang <95869708+saturnLang@users.noreply.github.com>
2022-06-18 19:25:58 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+var p1 = document.createElement("p");
+p1.innerHTML = "<p class='animelist'>Anime List</p>";
+document.body.appendChild(p1);
+var half = document.createElement("div");
+half.innerHTML = "<div class='half'></div>";
+document.body.appendChild(half);
